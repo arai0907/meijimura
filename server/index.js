455c162f6966f8335983cebea0fb1c0a81b8dce6
araisaaya <bump.rad.saaya97@gmail.com>
2019-07-11 17:13:24 +0900

    color

--- server/index.js
+++ server/index.js
@@ -31,11 +31,14 @@ const TRUE_COLORS = {
     ['id' + COLORS.purple]: [COLORS.red,COLORS.blue]
 };
 
+// 白と黒
+const colorsId = [COLORS.black,COLORS.white];
+const colorId = colorsId[Math.floor(Math.random() * colorsId.length)];
+
 let vote1ResultColorId; // 1回目の投票結果の色IDを保持する
 let vote2ResultColorId; // 2回目の投票結果の色IDを保持する
 
 const vote1colors = [] // 投票１でランダムで選択された2色のID
-const colorId = colorsId[Math.floor(Math.random() * colorsId.length)] ;
 
 const app = express();
 const httpServer = http.Server(app);
@@ -221,11 +224,10 @@ app.get('/api/vote/end/3', (req,res) => {
         });
         res.json({ colorId: COLORS.black });
     } else {
-        colorsId = [COLORS.black,COLORS.white];
         io.emit('/api/vote/end/3', { 
-            colorId: colorsId[Math.floor(Math.random() * colorsId.length)]
+            colorId: colorId
         });
-        res.json({ colorId: [Math.floor(Math.random() * colorsId.length)]  });
+        res.json({ colorId: colorId });
     }
     phase = '/api/vote/end/3';
     io.emit('phase', { phase: phase });
